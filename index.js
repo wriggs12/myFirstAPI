0e62c5643e34764370c13b09ec7e21c3c6f919e0
Winston Riggs <wriggs12@gmail.com>
2021-11-15 11:50:26 -0500

    added gitignore

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ require('dotenv/config');
 
 const port  = process.env.PORT || 3000;
 
+//Connect to DB
+mongoose.connect(process.env.DB_Connection).then((result) => app.listen(port)).catch((err) => console.log(err));
+
 //Middleware
 app.use(express.json());
 
 const carsRoute = require('./routes/cars');
 app.use('/cars', carsRoute);
-
-//Connect to DB
-mongoose.connect(process.env.DB_Connection).then((result) => app.listen(port)).catch((err) => console.log(err));

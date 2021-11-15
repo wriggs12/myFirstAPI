60023593707b7a52f9a9a2cdcc3a73263c94ce35
Winston Riggs <wriggs12@gmail.com>
2021-11-15 12:29:10 -0500

    hide db credentials

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv/config');
 const port  = process.env.PORT || 3000;
 
 //Connect to DB
-mongoose.connect('mongodb+srv://wriggs12:tnXnKw2fnYJ328gm@cars.tccrv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then((result) => app.listen(port)).catch((err) => console.log(err));
+mongoose.connect(process.env.DB_Connection).then((result) => app.listen(port)).catch((err) => console.log(err));
 
 //Middleware
 app.use(express.json());
